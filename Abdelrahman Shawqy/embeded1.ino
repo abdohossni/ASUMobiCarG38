3ce5ef4e3b2fb8c7120aa90cce471b71befa1f4e
AbdelRahman Shawqy <31411654+shawqy@users.noreply.github.com>
2018-02-18 22:50:26 +0200

    Add files via upload

---
+++ Abdelrahman Shawqy/embeded1.ino
@@ -0,0 +1,14 @@
+#define pls 8
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pls,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pls,HIGH);
+delay(500);
+digitalWrite(pls,LOW);
+delay(1500);
+
+}
