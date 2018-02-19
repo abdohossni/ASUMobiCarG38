f29c5c696160a4d518c72cc41cc8aadb4cb821c0
SamirMossad <36465768+SamirMossad@users.noreply.github.com>
2018-02-19 16:45:34 +0200

    Control Project sheet ( Arduino code)

---
+++ sketch_feb18a.ino
@@ -0,0 +1,55 @@
+#define Ledr 4
+#define Ledg 5
+#define Ledb 6
+#define Ledy 7
+
+void setup() {
+ 
+pinMode(Ledr,OUTPUT);
+pinMode(Ledg,OUTPUT);
+pinMode(Ledb,OUTPUT);
+pinMode(Ledy,OUTPUT);
+
+}
+
+
+
+void loop() {
+  ForwardOn();
+  BackwardOn();
+
+}
+
+
+void ForwardOn()
+{
+  digitalWrite(Ledr,1);
+  delay(1000);
+  digitalWrite(Ledr,0);
+  digitalWrite(Ledg,1);
+  delay(1000);
+  digitalWrite(Ledg,0);
+  digitalWrite(Ledb,1);
+  delay(1000);
+  digitalWrite(Ledb,0);
+  digitalWrite(Ledy,1);
+  delay(1000);
+  digitalWrite(Ledy,0);
+  delay(1000);
+  }
+void BackwardOn()
+{
+  digitalWrite(Ledy,1);
+  delay(1000);
+  digitalWrite(Ledy,0);
+  digitalWrite(Ledb,1);
+  delay(1000);
+  digitalWrite(Ledb,0);
+  digitalWrite(Ledg,1);
+  delay(1000);
+  digitalWrite(Ledg,0);
+  digitalWrite(Ledr,1);
+  delay(1000);
+  digitalWrite(Ledr,0);
+  delay(1000);
+  }
---
+++ sketch_feb18b.ino
@@ -0,0 +1,21 @@
+#define Pulse 1
+void setup() {
+
+pinMode(Pulse,OUTPUT);
+
+}
+
+void loop() {
+
+ Pulses(2000);
+
+}
+
+void Pulses(int TotalPeriod)
+{
+  digitalWrite(Pulse,1);
+  delay(TotalPeriod/4);
+  digitalWrite(Pulse,0);
+  delay((TotalPeriod*3)/4);  
+  }
+
